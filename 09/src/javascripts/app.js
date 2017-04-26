a13c46a3deb4d5b32d9be0ce5d12ad342b149afe
Łukasz Krawczykowski <lukaszkrawczykowski@gmail.com>
2017-04-26 10:01:43 +0200

    assignment-09-updated

--- 09/src/javascripts/app.js
+++ 09/src/javascripts/app.js
@@ -1,24 +1,19 @@
 let wd = require('./wd.js');
 
 window.onload = function () {
-
-    console.log(wd);
-
-    var application = wd
+    const application = wd
         .createRoot('app');
-
-    console.log(application);
 
     application.routes([
         { url: '/', templateUrl: 'routes/home.html' },
         { url: '/sign-in', templateUrl: 'routes/sign-in.html' }
     ]);
 
-    //wd.root('app').navigate('/');
+    wd.root('app').navigate('/');
 
     setTimeout(() => {
-        //wd.root('app').navigate('/sign-in');
+        wd.root('app').navigate('/sign-in');
     }, 3000);
 
-    //wd.createRoot('appB');
+    wd.createRoot('appB');
 }
